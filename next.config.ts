b52258e3ea6dc8e1c6a03b1a312b5e1d6961a93f
ColdFusionX Studio <140541798+ColdFusionXStudio@users.noreply.github.com>
2025-12-28 21:56:23 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,53 +1,48 @@
 
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'picsum.photos',
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'encrypted-tbn0.gstatic.com',
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'i.scdn.co',
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'image-cdn-ak.spotifycdn.com',
-        port: '',
         pathname: '/**',
-      }
+      },
     ],
   },
-  devIndicators: {
-    allowedDevOrigins: [
-      '*.cluster-wfwbjypkvnfkaqiqzlu3ikwjhe.cloudworkstations.dev',
-    ],
-  },
-};
+}
 
-export default nextConfig;
+export default nextConfig
+
