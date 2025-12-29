df60f6d4a88d08e3a23b89c2d197d0cab293f826
ColdFusionX Studio <140541798+ColdFusionXStudio@users.noreply.github.com>
2025-12-28 21:23:09 -0600

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,53 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.scdn.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image-cdn-ak.spotifycdn.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  devIndicators: {
+    allowedDevOrigins: [
+      '*.cluster-wfwbjypkvnfkaqiqzlu3ikwjhe.cloudworkstations.dev',
+    ],
+  },
+};
+
+export default nextConfig;
